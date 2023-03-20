30e46c53fe1b9da73ca7741bdeec8198490d38b7
Marc Foley <m.foley.88@gmail.com>
2023-03-20 14:45:28 +0000

    test test strings

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,47 +1,63 @@
-from glyphsets import _GFGlyphData
+from glyphsets import _GFGlyphData, _TestDocData
 from fontTools.ttLib import TTFont
 import os
 import pytest
+from copy import deepcopy as copy
 
 DATA_FP = os.path.join(os.path.dirname(__file__), "data")
+
+glyphs_data = {
+    "glyphs": [
+        {
+            "nice_name": "A",
+            "production_name": "A",
+            "character": "A",
+            "unicode": 65,
+            "glyphsets": [
+                "GFLatinIPA",
+                "GFLatinAfrican",
+                "GFLatinCore",
+                "GFLatinKernel",
+                "GFLatinMinorities",
+                "GFLatinPlus",
+                "GFLatinVietnamese",
+            ],
+        },
+        {
+            "nice_name": "brevecomb_acutecomb",
+            "production_name": "uni03060301",
+            "character": None,
+            "unicode": None,
+            "glyphsets": ["GFLatinVietnamese"],
+        },
+        {
+            "nice_name": "fdotaccent",
+            "production_name": "uni1E1F",
+            "character": "\u1e1f",
+            "unicode": 7711,
+            "glyphsets": ["GFLatinAfrican"],
+        },
+    ]
+}
 
 
 @pytest.fixture
 def glyph_data():
+    return _GFGlyphData(glyphs_data)
+
+
+@pytest.fixture
+def test_string_data():
     data = {
-        "glyphs": [
-            {
-                "nice_name": "A",
-                "production_name": "A",
-                "character": "A",
-                "unicode": 65,
-                "glyphsets": [
-                    "GFLatinIPA",
-                    "GFLatinAfrican",
-                    "GFLatinCore",
-                    "GFLatinKernel",
-                    "GFLatinMinorities",
-                    "GFLatinPlus",
-                    "GFLatinVietnamese",
-                ],
-            },
-            {
-                "nice_name": "brevecomb_acutecomb",
-                "production_name": "uni03060301",
-                "character": None,
-                "unicode": None,
-                "glyphsets": ["GFLatinVietnamese"],
-            },
-            {
-                "nice_name": "fdotaccent",
-                "production_name": "uni1E1F",
-                "character": "\u1e1f",
-                "unicode": 7711,
-                "glyphsets": ["GFLatinAfrican"],
-            },
-        ]
+        "GFLatinKernel": {
+            "Spacing": ["AAA"],
+        },
+        "GFLatinAfrican": {
+            "Spacing": ["BBB"],
+        }
+
     }
-    return _GFGlyphData(data)
+    return _TestDocData(data, _GFGlyphData(glyphs_data))
 
 
 @pytest.fixture
