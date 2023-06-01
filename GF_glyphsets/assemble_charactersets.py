2c00f0d64c25a3fa686ab4ac980e08a8982e78fd
Yanone <post@yanone.de>
2023-06-01 16:19:33 +0200

    Initial commit

---
+++ GF_glyphsets/assemble_charactersets.py
@@ -0,0 +1,66 @@
+import yaml
+import os
+import gflanguages
+import unicodedata
+
+
+def assemble_characterset(nam_stub_path, nam_path, languages_yaml_path):
+    character_set = set()
+
+    # Read language definitions
+    with open(languages_yaml_path, "r") as stream:
+        language_definitions = yaml.safe_load(stream)
+
+    # Assemble character sets from gflanguages
+    languages = gflanguages.LoadLanguages()
+    for language_code in language_definitions["language_codes"]:
+        chars = languages[language_code].exemplar_chars
+        character_set.update(
+            {
+                ord(c)
+                for c in list(
+                    set(chars.base)
+                    | set(chars.index)
+                    | set(chars.marks)
+                    | set(chars.numerals)
+                    | set(chars.punctuation)
+                    # | set(chars.auxiliary)
+                )
+                if c not in (" ", "{", "}")
+            }
+        )
+
+    # Read .nam stub file
+    with open(nam_stub_path, "r") as f:
+        nam_stub_lines = f.readlines()
+    for line in nam_stub_lines:
+        unicode = line.split(" ")[0]
+        if unicode.startswith("0x"):
+            character_set.add(int(unicode[2:], 16))
+
+    # Output sorted character set to .nam file
+    with open(nam_path, "w") as f:
+        for unicode in sorted(list(character_set)):
+            unicode_string = f"{unicode:#0{6}X}".replace("0X", "0x")
+            f.write(f"{unicode_string} {unicodedata.name(chr(unicode))}\n")
+
+
+if __name__ == "__main__":
+    for root, dir, files in os.walk(os.path.dirname(__file__)):
+        for file in files:
+            if file.endswith(".stub.nam"):
+                # .nam stub file
+                nam_stub_path = os.path.join(root, file)
+                nam_path = os.path.join(root, file).replace(".stub.nam", ".nam")
+
+                # corresponding languages .yaml file
+                languages_yaml_path = os.path.abspath(
+                    os.path.join(
+                        root, "..", "languages", file.replace(".stub.nam", ".yaml")
+                    )
+                )
+
+                if all(
+                    [os.path.exists(x) for x in [nam_stub_path, languages_yaml_path]]
+                ):
+                    assemble_characterset(nam_stub_path, nam_path, languages_yaml_path)
---
+++ GF_glyphsets/assemble_languages.py
@@ -0,0 +1,33 @@
+import gflanguages
+
+regions = gflanguages.LoadRegions()
+languages = gflanguages.LoadLanguages()
+
+SPEAKERS = 5000000
+
+if __name__ == "__main__":
+    country_codes = set()
+
+    # Go through countries and add them to the set if they are in Europe or Americas
+    for country_code in regions:
+        if set(regions[country_code].region_group).intersection(
+            set(("Europe", "Americas"))
+        ):
+            country_codes.add(country_code)
+
+    # Go through languages and add them to the set if they are in Europe or Americas
+    language_codes = set()
+
+    # Print languages if they are supported through the logic
+    for language_code in sorted(languages.keys()):
+        if (
+            set(languages[language_code].region).intersection(country_codes)
+            and languages[language_code].population > SPEAKERS
+            and languages[language_code].script == "Latn"
+        ):
+            if not languages[language_code].exemplar_chars:
+                print(
+                    f"WARNING, {languages[language_code].name} has no character defintions."
+                )
+            language_codes.add(language_code)
+            print(f"  - {language_code}  # {languages[language_code].name}")
