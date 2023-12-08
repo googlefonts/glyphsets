b4e3003bbcbac6412774cc71073aaca1dd3f9a3a
Yanone <post@yanone.de>
2023-12-08 14:03:53 +0100

    Language defs

--- Lib/glyphsets/definitions/__init__.py
+++ Lib/glyphsets/definitions/__init__.py
@@ -37,7 +37,25 @@ glyphset_definitions = {
             "sv_Latn",  # Swedish
             "tr_Latn",  # Turkish
         ],
-    }
+    },
+    "GF_Arabic_Core": {
+        "script": "Arabic",
+        "language_codes": [
+            "ar_Arab",  # Arabic
+            "fa_Arab",  # Persian
+            "ur_Arab",  # Urdu
+        ],
+    },
+    "GF_Arabic_Plus": {
+        "script": "Arabic",
+        "language_codes": [
+            "ckb_Arab",  # Kurdish
+            "ms_Arab",  # Malay
+            "ps_Arab",  # Pashto
+            "ps_Arab",  # Sindhi
+            "ug_Arab",  # Uyghur
+        ],
+    },
 }
 
 
