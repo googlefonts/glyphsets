67ba013fc79e941ee7450cc5cc510a9cfab8560a
Yanone <post@yanone.de>
2023-12-13 16:15:22 +0100

    Updated check

--- tests/test_glyphsets.py
+++ tests/test_glyphsets.py
@@ -2,4 +2,4 @@ from glyphsets.definitions import unicodes_per_glyphset
 
 
 def test_definitions():
-    assert len(unicodes_per_glyphset("GF_Latin_Core")) == 328
+    assert len(unicodes_per_glyphset("GF_Latin_Core")) == 319
