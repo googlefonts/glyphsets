ab0774f1261fcbed419189936e6190a013d65012
Yanone <post@yanone.de>
2024-02-14 10:48:50 +0100

    And this...

--- GF_glyphsets/update-gs.sh
+++ GF_glyphsets/update-gs.sh
@@ -1,35 +1,38 @@
-#!/bin/sh
-set -e
+# Deactivated this because it's now superseded by build.sh
 
-# Go the sources directory to run commands
-SOURCE="${BASH_SOURCE[0]}"
-DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-cd $DIR
 
-echo "clean-up"
-rm -rf ./*/nam/*.nam ./*/txt/*/*.txt ./*/glyphs/*.plist
+# #!/bin/sh
+# set -e
 
-echo "Updating lists"
+# # Go the sources directory to run commands
+# SOURCE="${BASH_SOURCE[0]}"
+# DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+# cd $DIR
 
-scripts=$(ls -d */ | cut -f1 -d'/')
+# echo "clean-up"
+# rm -rf ./*/nam/*.nam ./*/txt/*/*.txt ./*/glyphs/*.plist
 
-for script in $scripts
-do
-    echo $script
-    sets=$(ls ./$script/glyphs/*.glyphs | xargs -n 1 basename | sed -e 's/\.glyphs$//')
-    if [[ $sets == *GF_Latin_Core* ]]
-    then
-        glyphsets filter-list $sets -o $script/glyphs/CustomFilter_GF_$script.plist
-    elif [[ $sets == *GF_TransLatin* ]]
-    then
-        glyphsets filter-list GF_Latin_Kernel $sets -o $script/glyphs/CustomFilter_GF_$script.plist
-    else
-        glyphsets filter-list GF_Latin_Core $sets -o $script/glyphs/CustomFilter_GF_$script.plist
-    fi
-    for set in $sets
-    do
-        glyphsets nam-file $set -o $script/nam/$set.nam
-        glyphsets filter-list $set -o $script/txt/nice-names/$set.txt
-        glyphsets filter-list $set -o $script/txt/prod-names/$set.txt --prod-names
-    done
-done
+# echo "Updating lists"
+
+# scripts=$(ls -d */ | cut -f1 -d'/')
+
+# for script in $scripts
+# do
+#     echo $script
+#     sets=$(ls ./$script/glyphs/*.glyphs | xargs -n 1 basename | sed -e 's/\.glyphs$//')
+#     if [[ $sets == *GF_Latin_Core* ]]
+#     then
+#         glyphsets filter-list $sets -o $script/glyphs/CustomFilter_GF_$script.plist
+#     elif [[ $sets == *GF_TransLatin* ]]
+#     then
+#         glyphsets filter-list GF_Latin_Kernel $sets -o $script/glyphs/CustomFilter_GF_$script.plist
+#     else
+#         glyphsets filter-list GF_Latin_Core $sets -o $script/glyphs/CustomFilter_GF_$script.plist
+#     fi
+#     for set in $sets
+#     do
+#         glyphsets nam-file $set -o $script/nam/$set.nam
+#         glyphsets filter-list $set -o $script/txt/nice-names/$set.txt
+#         glyphsets filter-list $set -o $script/txt/prod-names/$set.txt --prod-names
+#     done
+# done
--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-sh GF_glyphsets/update-gs.sh
 python scripts/assemble_charactersets.py
 python scripts/assemble_description.py
